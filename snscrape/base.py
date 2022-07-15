48286fede23519f00e3ff1a7747aa0458d582cc9
Marek294 <Markra5835@gmail.com>
2022-07-15 08:42:37 +0200

    restore version from fork master

--- snscrape/base.py
+++ snscrape/base.py
@@ -5,14 +5,12 @@ import datetime
 import functools
 import json
 import logging
-import random
-
-import httpx
+import requests
 import time
 import warnings
-import retrying
-logger = logging.getLogger(__name__)
 
+
+logger = logging.getLogger(__name__)
 
 
 class _DeprecatedProperty:
@@ -122,12 +120,10 @@ class URLItem(Item):
 	'''A generic item which only holds a URL string.'''
 
 	def __init__(self, url):
-
 		self._url = url
 
 	@property
 	def url(self):
-
 		return self._url
 
 	def __str__(self):
@@ -143,18 +139,10 @@ class Scraper:
 
 	name = None
 
-	def __init__(self, retries=3, proxy=''):
+	def __init__(self, *, retries = 3, proxies = None):
 		self._retries = retries
-		if not proxy:
-			self._client = httpx.Client(http2=True, verify=True)
-		else:
-			self._proxy = proxy
-			random.shuffle(proxy)
-			x = random.choice(proxy)
-			proxies = {'http://': f'http://{x}',
-					   'https://': f'http://{x}',
-					   }
-			self._client = httpx.Client(http2=True, proxies=proxies, verify=True)
+		self._proxies = proxies
+		self._session = requests.Session()
 
 	@abc.abstractmethod
 	def get_items(self):
@@ -174,17 +162,21 @@ class Scraper:
 	def entity(self):
 		return self._get_entity()
 
-	def _request(self, method, url, params = None, data = None, headers = None, timeout = 10, responseOkCallback = None, allowRedirects = True):
+	def _request(self, method, url, params = None, data = None, headers = None, timeout = 10, responseOkCallback = None, allowRedirects = True, proxies = None):
+		proxies = proxies or self._proxies or {}
 		for attempt in range(self._retries + 1):
 			# The request is newly prepared on each retry because of potential cookie updates.
-			req = self._client.get('https://api.myip.com', params=params, headers=headers)
+			req = self._session.prepare_request(requests.Request(method, url, params = params, data = data, headers = headers))
+			environmentSettings = self._session.merge_environment_settings(req.url, proxies, None, None, None)
 			logger.info(f'Retrieving {req.url}')
 			logger.debug(f'... with headers: {headers!r}')
 			if data:
 				logger.debug(f'... with data: {data!r}')
+			if environmentSettings:
+				logger.debug(f'... with environmentSettings: {environmentSettings!r}')
 			try:
-				r = self._client.get(url, params=params, headers=headers, timeout=timeout)
-			except httpx.RequestError as exc:
+				r = self._session.send(req, allow_redirects = allowRedirects, timeout = timeout, **environmentSettings)
+			except requests.exceptions.RequestException as exc:
 				if attempt < self._retries:
 					retrying = ', retrying'
 					level = logging.INFO
@@ -232,15 +224,15 @@ class Scraper:
 		return self._request('POST', *args, **kwargs)
 
 	@classmethod
-	def cli_setup_parser(cls, subparser):
+	def _cli_setup_parser(cls, subparser):
 		pass
 
 	@classmethod
-	def cli_from_args(cls, args):
-		return cls._construct(args)
+	def _cli_from_args(cls, args):
+		return cls._cli_construct(args)
 
 	@classmethod
-	def cli_construct(cls, argparseArgs, *args, **kwargs):
+	def _cli_construct(cls, argparseArgs, *args, **kwargs):
 		return cls(*args, **kwargs, retries = argparseArgs.retries)
 
 
